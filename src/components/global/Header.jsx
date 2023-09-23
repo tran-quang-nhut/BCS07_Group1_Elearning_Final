00cbf52500d0236e0e093037f2a71a1b3e424641
Tran Quang Nhut <ntran64634@gmail.com>
2023-09-23 11:52:08 +0700

    sua ten

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -62,7 +62,7 @@ const Header = () => {
         <div className="container mx-auto flex justify-between items-center ">
           <NavLink to='/' className={styles.Logo} >
             <img src={require('../../assets/logo/logo-tree.png')} alt="logo E-learning" />
-            <h1>Nhom1 E-LEARNING</h1>
+            <h1>Nhóm 1 E-LEARNING</h1>
           </NavLink>
 
           <nav id='openNav' className={clsx(styles.navMenu, 'md:translate-x-0',
